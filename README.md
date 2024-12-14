039f50122802afa42bd0d96434602c85158200d0
rxanders35 <reidxanders@outlook.com>
2024-12-14 15:09:05 -0500

    project init

---
+++ README.md
@@ -0,0 +1,2 @@
+### HYDRA TABLE ###
+The plan for this project is to create a distributed hashtable that supports auto-resizing and re-partitioning.
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use std::collections::HashMap;
+
+struct HydraStore {
+    store: HashMap<String, String>,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
